a57fe83aad23f0c974c928dc3f87a5974bfbf2a7
Tiger-089 <Tiger-089@thoughtworks.com>
2013-07-24 12:19:02 +0800

    Step 2: clean up

---
+++ AspNetWebApiDemo/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+
+namespace AspNetWebApiDemo
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration configuration)
+        {
+            ConfigureHttpRoutes(configuration.Routes);
+        }
+
+        private static void ConfigureHttpRoutes(HttpRouteCollection routes)
+        {
+            routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+        }
+    }
+}
